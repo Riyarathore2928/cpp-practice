091f6741f6044f7d0e926d8dc0db522fa48db3ff
Riya Rathore <riyarathore292007@gmail.com>
2026-07-26 16:49:16 +0530

    Fix Hello.cpp - restore correct hello world program

--- Hello.cpp
+++ Hello.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 
 int main() {
-    int n, sum = 0;
-    cin >> n;
-
-    while ( n != 0) {
-        int digit = n % 10;
-        sum = digit + sum;
-        n = n / 10;
-    }
-    cout << "The sum of the digits is: " << sum << endl;
+    cout << "Namaste Duniya" << endl;
     return 0;
 }
