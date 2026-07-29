86aa0ccf7eed8ca94d5b4437cababdecb20fe9a6
Riya Rathore <riyarathore292007@gmail.com>
2026-07-29 23:37:41 +0530

    Add data types practice programs

---
+++ variables & data types/datatypes.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    //variable define
+    //int age = 25;
+    // cout << Riya's Age is: " << age << endl;
+    
+    //decalartion
+    // int age;
+    // cout << age;
+    
+    //definition
+    int age = 25;
+    cout << age << endl;
+
+    //manipulation or updation
+    age = 101;
+    cout << age << endl;
+
+
+    //int
+    int count = 6;
+    //float
+    float share = 3.14;
+    //char
+    char alphabet = 'z';
+    //double
+    double weight = 55.6987;
+    //boolean;
+    bool isStudent = true;
+    bool isChild = 1;
+    bool isFemale = false;
+    bool isGirl = 0;
+
+    cout << count << endl;
+    cout << share << endl;
+    cout << alphabet << endl;
+    cout << weight << endl;
+    cout << isStudent << endl;
+    cout << isChild << endl;
+    cout << isFemale << endl;
+    cout << isGirl << endl;
+
+    return 0;
+
+}
