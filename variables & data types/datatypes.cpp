db07d8bbb88cc9b3d3a51548491ffe382c85beb8
Riya Rathore <riyarathore292007@gmail.com>
2026-08-01 00:21:54 +0530

    Add operators practice program(+=, -=, *=)

--- variables & data types/datatypes.cpp
+++ variables & data types/datatypes.cpp
@@ -1,41 +1,22 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
 int main() {
-    //variable define
-    //int rollNumber = 101;
-    //cout << "roll number is" << RollNumber  << endl;
+    //veraible define
+    int score = 10;
+    cout <<  score  << endl;
 
-    //decalaration
-    //int rollNumber;
-    //cout << rollNumber;
+    //add  5
+    score += 5;
+    cout << score << endl;
 
-    //definition
-    //int rollNumber;
-    //cout << rollnumber << endl;
+    //subtarct 2
+    score -= 2;
+    cout << score << endl;
 
-    //manipulation or updation
-    int rollNumber = 101;
-    cout << rollNumber << endl;
-
-    //int 
-    int count = 101;
-    //float
-    float percentage = 85.5;
-    //char
-    char grade = 'A';
-    //boolean
-    bool isPassed = 1;
-    bool isStudent = true;
-    //string
-    string studentName = "Riya";
-
-    cout << "Roll Number" << rollNumber << endl;
-    cout << "Percentage" << percentage << endl;
-    cout << "Grade" << grade << endl;
-    cout << "Passed" << isPassed << endl;
-    cout << "Name" << studentName << endl;
+    //multiply by 2;
+    score *= 2;
+    cout << score << endl;
 
     return 0;
 }
