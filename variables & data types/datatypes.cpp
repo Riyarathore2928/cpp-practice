d4a0f0eaa26bfbecacd97a18d253123700614f2b
Riya Rathore <riyarathore292007@gmail.com>
2026-08-08 22:30:16 +0530

    Add marks input program

--- variables & data types/datatypes.cpp
+++ variables & data types/datatypes.cpp
@@ -2,21 +2,13 @@
 using namespace std;
 
 int main() {
-    //veraible define
-    int score = 10;
-    cout <<  score  << endl;
+    //variable declare
+    int marks;
+    cout << "Enter your marks" << endl;
 
-    //add  5
-    score += 5;
-    cout << score << endl;
-
-    //subtarct 2
-    score -= 2;
-    cout << score << endl;
-
-    //multiply by 2;
-    score *= 2;
-    cout << score << endl;
+    //input
+    cin >> marks;
+    cout << "Riya'smarks: " << marks << endl;
 
     return 0;
 }
