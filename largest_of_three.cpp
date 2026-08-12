c889dbca53af5a735e37dcad245492b3911a8d57
Riya Rathore <riyarathore292007@gmail.com>
2026-08-12 22:44:20 +0530

    Added arithmetic operators program

--- largest_of_three.cpp
+++ largest_of_three.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    
+
     if (a >= b && a >= c) {
-        cout << a << " is the largest number" << endl;
+        cout << a << " is the largest number " << endl;
     } else if (b >= a && b >= c) {
-        cout << b << " is the largest number" << endl;
+        cout << b << " is the largest number " << endl;
     } else {
-        cout << c << " is the largest number" << endl;
+        cout << c << " is the largest number " << endl;
     }
-    
     return 0;
 }
